c9a7e4172266bc495814b9d2eedd4e6f31861a60
Vladislav <104913853+Adwasar@users.noreply.github.com>
2024-07-22 17:22:17 +0300

    lesston: fonts

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -2,6 +2,8 @@ import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 
 export default function InvoiceStatus({ status }: { status: string }) {
+  
+  
   return (
     <span
       className={clsx(
